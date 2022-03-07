526f201cb3bc85a92e06fb9f905b59c113d57a34
谢雄雄 <1320285772@qq.com>
2022-03-07 16:25:47 +0800

    test

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+   cout << "Hello World!";
+   int *p = x;
+   char * second_char = (char *)(p + 1);
+    printf("%s", NULL)
+   return 0;
+}
